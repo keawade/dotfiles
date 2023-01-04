9d2a10e5740100c719ca5f4a6cbdc5cc086762a9
Keith Wade <keith.wade@nelnet.net>
2023-01-04 12:08:29 -0500

    Add fun MOTD

---
+++ .config/fish/functions/fish_greeting.fish
@@ -0,0 +1,9 @@
+function fish_greeting
+  # Clear output from shell `login`. `.hushlogin` works too but needs to be in the directory being opened to work and this works everywhere.    
+  # Piping to /dev/null because I've aliased `clear` to print some fun text and this gets rid of it in this instance
+  clear > /dev/null
+  echo Welcome to fish, the friendly interactive shell 🐟
+  echo
+  fortune -s
+end
+

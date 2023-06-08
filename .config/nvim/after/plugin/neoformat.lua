21fa95deb1d563ec31269a31bc3c6613a0dbc253
Keith Wade <keith.wade@nelnet.net>
2023-06-08 13:56:04 -0400

    Override neofmt default order for TS

--- .config/nvim/after/plugin/neoformat.lua
+++ .config/nvim/after/plugin/neoformat.lua
@@ -2,6 +2,8 @@
 vim.g.neoformat_try_node_exe = 1
 -- Override JS default formatter order
 vim.g.neoformat_enabled_javascript = { 'prettierd', 'prettier', 'jsbeautify', 'standard', 'semistandard', 'prettydiff', 'clangformat', 'esformatter', 'prettiereslint', 'eslint_d', 'denofmt' }
+-- Override TS default formatter order
+vim.g.neoformat_enabled_javascript = { 'prettierd', 'prettier', 'prettiereslint', 'tsfmt', 'tslint', 'eslint_d', 'clangformat', 'denofmt' }
 -- Override JSON default formatter order
 vim.g.neoformat_enabled_json = { 'prettierd', 'prettier', 'jsbeautify', 'prettydiff', 'jq', 'fixjson', 'denofmt' }
 

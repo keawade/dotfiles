c198a0fa59a85f3bca200cf7ed2b2a2d2aed977f
Keith Wade <keawade@protonmail.com>
2023-10-09 19:19:30 -0400

    Add ts -> tmux-sessionizer alias, indentation change

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,42 +1,40 @@
 if status is-interactive
-  # Set up dotfiles git command
-  alias config='/usr/bin/git --git-dir=$HOME/.keawade-config/ --work-tree=$HOME'
+    # Set up dotfiles git command
+    alias config='/usr/bin/git --git-dir=$HOME/.keawade-config/ --work-tree=$HOME'
 
-  # Initialize starship prompt
-  starship init fish | source
+    # Initialize starship prompt
+    starship init fish | source
 
-  # Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
-  complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+    # Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
+    complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 
-  # Initialize k8s completions
-  kubectl completion fish | source
+    # Initialize k8s completions
+    kubectl completion fish | source
 
-  # Select node version to use with nvm for session
-  nvm use 18 > /dev/null
+    # Select node version to use with nvm for session
+    nvm use 18 >/dev/null
 
-  # Be more polite
-  alias f='fuck'
+    # Be more polite
+    alias f='fuck'
 
-  # Override a few coreutils with fancier versions
-  alias ls='exa'
-  #alias cat='bat'
-  alias find='fd'
-  alias vim='nvim'
+    # Override a few coreutils with fancier versions
+    alias ls='exa'
+    alias vim='nvim'
+    alias ts='tmux-sessionizer'
 
-  set -gx THEFUCK_OVERRIDEN_ALIASES 'ls,find,vim'
+    set -gx THEFUCK_OVERRIDEN_ALIASES 'ls,find,vim'
 
-  alias kc="kubectl"
-  alias watchpods="watch -n 1 kubectl get pods"
-  alias decapitate="helm ls --all --short | xargs -L1 helm delete"
-  alias rdrestart="rdctl shutdown && rdctl start"
+    alias kc="kubectl"
+    alias watchpods="watch -n 1 kubectl get pods"
+    alias decapitate="helm ls --all --short | xargs -L1 helm delete"
+    alias rdrestart="rdctl shutdown && rdctl start"
 
-  function on_exit --on-event fish_exit
-    echo 'so long and thanks for all the fish 🐬'
-    sleep 0.5
-  end
+    function on_exit --on-event fish_exit
+        echo 'so long and thanks for all the fish 🐬'
+        sleep 0.5
+    end
 
-  alias clear="clear && echo 'so clean ✨'"
+    alias clear="clear && echo 'so clean ✨'"
 
-  alias :q="exit"
+    alias :q="exit"
 end
-
